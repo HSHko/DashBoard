8256c59c861b4e5828b814b6c1e4d2a5caff928c
HSH <pleasmin@gmail.com>
2020-10-23 10:56:23 +0900

    restoring corrupted files..

--- src/logged_out/containers/index.js
+++ src/logged_out/containers/index.js
@@ -1,3 +1,3 @@
-export { default as NavBar } from "./navigation/NavBar";
+export { default as NavBar } from "./navigation/Navbar";
 export { default as Footer } from "./footer/Footer";
 export { default as Dialog } from "./dialog/Dialog";
